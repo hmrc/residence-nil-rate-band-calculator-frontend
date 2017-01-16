59950896656e0ce8ef342863de44067ec1e43989
Nick Wilson <thenickwilson@gmail.com>
2017-01-16 13:14:59 +0000

    RNRBC-108 WIP on exit questionnaire page

---
+++ test/uk/gov/hmrc/residencenilratebandcalculator/controllers/ExitQuestionnaireControllerSpec.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.residencenilratebandcalculator.controllers
+
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.test.WithFakeApplication
+import uk.gov.hmrc.residencenilratebandcalculator.Navigator
+import uk.gov.hmrc.residencenilratebandcalculator.views.HtmlSpec
+import uk.gov.hmrc.residencenilratebandcalculator.views.html.exit_questionnaire
+
+class ExitQuestionnaireControllerSpec extends HtmlSpec with WithFakeApplication {
+
+  val fakeRequest = FakeRequest("", "")
+
+  val navigator = injector.instanceOf[Navigator]
+
+
+  "Exit Questionnaire controller" must {
+
+    "return 200 for a GET" in {
+      val result = new ExitQuestionnaireController(frontendAppConfig, messagesApi, navigator).onPageLoad()(fakeRequest)
+      status(result) shouldBe 200
+    }
+
+    "return the View for a GET" in {
+      val result = new ExitQuestionnaireController(frontendAppConfig, messagesApi, navigator).onPageLoad()(fakeRequest)
+      contentAsString(result) shouldBe exit_questionnaire(frontendAppConfig)(fakeRequest, messages).toString
+    }
+  }
+}
---
+++ test/uk/gov/hmrc/residencenilratebandcalculator/views/ExitQuestionnaireViewSpec.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.residencenilratebandcalculator.views
+
+import uk.gov.hmrc.residencenilratebandcalculator.views.html.exit_questionnaire
+
+class ExitQuestionnaireViewSpec extends ViewSpecBase {
+
+  val messageKeyPrefix = "exit_questionnaire"
+
+  "Exit Questionnaire view" must {
+    "display the correct browser title" in {
+      val doc = asDocument(exit_questionnaire(frontendAppConfig)(request, messages))
+      assertEqualsMessage(doc, "title", s"$messageKeyPrefix.browser_title")
+    }
+  }
+}
