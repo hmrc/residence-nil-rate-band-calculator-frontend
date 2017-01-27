789ba97b792d4fe92920d2440d555cc0d9446f85
Andy Dwelly <andydwelly@gmail.com>
2017-01-27 13:47:19 +0000

    RNRBC-114: Implemented an example of session data removal

--- app/uk/gov/hmrc/residencenilratebandcalculator/connectors/SessionConnector.scala
+++ app/uk/gov/hmrc/residencenilratebandcalculator/connectors/SessionConnector.scala
@@ -18,23 +18,31 @@ package uk.gov.hmrc.residencenilratebandcalculator.connectors
 
 import javax.inject.Inject
 
-import play.api.libs.json.Reads
+import play.api.libs.json.{JsValue, Json, Reads, Writes}
 import uk.gov.hmrc.http.cache.client.{CacheMap, SessionCache}
 import javax.inject.{Inject, Singleton}
-import play.api.libs.json.{Json, Writes}
+
 import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.play.http.HeaderCarrier
+import uk.gov.hmrc.residencenilratebandcalculator.Constants
 import uk.gov.hmrc.residencenilratebandcalculator.repositories.SessionRepository
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @Singleton
 class SessionConnector @Inject()(val sessionRepository: SessionRepository) {
-  var funcMap: Map[String, CacheMap => CacheMap] = Map()
+  var funcMap: Map[String, (JsValue, CacheMap) => CacheMap] = Map(Constants.propertyValueId -> ((v, cm) => propertyValueClearance(v, cm)))
+
+  private def propertyValueClearance[A](value: A, cacheMap: CacheMap)(implicit wrts: Writes[A]): CacheMap = {
+    val keysToRemove = Set(Constants.percentageCloselyInheritedId, Constants.anyExemptionId, Constants.propertyValueAfterExemptionId)
+    val mapWithDeletedKeys = cacheMap copy (data = cacheMap.data.filterKeys(s => !keysToRemove.contains(s)))
+    val xxxx = mapWithDeletedKeys copy (data = mapWithDeletedKeys.data + (Constants.propertyValueId -> Json.toJson(value)))
+    xxxx
+  }
 
   private def updateCacheMap[A](key: String, value: A, originalCacheMap: CacheMap)(implicit wts: Writes[A]): Future[CacheMap] = {
-    val newCacheMap = funcMap.get(key).fold(originalCacheMap copy (data = originalCacheMap.data + (key -> Json.toJson(value)))) { fn => fn(originalCacheMap)}
+    val newCacheMap = funcMap.get(key).fold(originalCacheMap copy (data = originalCacheMap.data + (key -> Json.toJson(value)))) { fn => fn(Json.toJson(value), originalCacheMap)}
     sessionRepository().upsert(newCacheMap).map {_ => newCacheMap}
   }
 
@@ -81,7 +89,7 @@ class SessionConnector @Inject()(val sessionRepository: SessionRepository) {
     }
   }
 
-  def associateFunc(key: String, fn: CacheMap => CacheMap) = {
+  def associateFunc(key: String, fn: (JsValue, CacheMap) => CacheMap) = {
     funcMap = funcMap + (key -> fn)
   }
 }
