1778f642ab074c1d0b0549669bc89d0a2d513cc4
Nick Wilson <thenickwilson@gmail.com>
2017-02-17 11:36:53 +0000

    Refactor

--- test/uk/gov/hmrc/residencenilratebandcalculator/models/UserAnswersSpec.scala
+++ test/uk/gov/hmrc/residencenilratebandcalculator/models/UserAnswersSpec.scala
@@ -24,108 +24,110 @@ import uk.gov.hmrc.residencenilratebandcalculator.Constants
 
 class UserAnswersSpec extends UnitSpec {
 
+  val cacheMapKey = "aa"
+  
   "User Answers" when {
 
     "values exist in the cache map" must {
 
       "return the correct answer for Any Assets Passing To Direct Descendant" in {
-        val cacheMap = CacheMap("a", Map(Constants.anyAssetsPassingToDirectDescendantsId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.anyAssetsPassingToDirectDescendantsId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.anyAssetsPassingToDirectDescendants shouldBe Some(true)
       }
 
       "return the correct answer for Any Brought Forward Allowance" in {
-        val cacheMap = CacheMap("a", Map(Constants.anyBroughtForwardAllowanceId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.anyBroughtForwardAllowanceId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.anyBroughtForwardAllowance shouldBe Some(true)
       }
 
       "return the correct answer for Any Brought Forward Allowance On Disposal" in {
-        val cacheMap = CacheMap("a", Map(Constants.anyBroughtForwardAllowanceOnDisposalId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.anyBroughtForwardAllowanceOnDisposalId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.anyBroughtForwardAllowanceOnDisposal shouldBe Some(true)
       }
 
       "return the correct answer for Any Downsizing Allowance" in {
-        val cacheMap = CacheMap("a", Map(Constants.anyDownsizingAllowanceId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.anyDownsizingAllowanceId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.anyDownsizingAllowance shouldBe Some(true)
       }
 
       "return the correct answer for Any Exemption" in {
-        val cacheMap = CacheMap("a", Map(Constants.anyExemptionId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.anyExemptionId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.anyExemption shouldBe Some(true)
       }
 
       "return the correct answer for Any Property Closely Inherited" in {
-        val cacheMap = CacheMap("a", Map(Constants.anyPropertyCloselyInheritedId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.anyPropertyCloselyInheritedId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.anyPropertyCloselyInherited shouldBe Some(true)
       }
 
       "return the correct answer for Assets Passing To Direct Descendant" in {
-        val cacheMap = CacheMap("a", Map(Constants.assetsPassingToDirectDescendantsId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.assetsPassingToDirectDescendantsId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.assetsPassingToDirectDescendants shouldBe Some(1)
       }
 
       "return the correct answer for Brought Forward Allowance" in {
-        val cacheMap = CacheMap("a", Map(Constants.broughtForwardAllowanceId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.broughtForwardAllowanceId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.broughtForwardAllowance shouldBe Some(1)
       }
 
       "return the correct answer for Brought Forward Allowance On Disposal" in {
-        val cacheMap = CacheMap("a", Map(Constants.broughtForwardAllowanceOnDisposalId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.broughtForwardAllowanceOnDisposalId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.broughtForwardAllowanceOnDisposal shouldBe Some(1)
       }
 
       "return the correct answer for Chargeable Transfer Amount" in {
-        val cacheMap = CacheMap("a", Map(Constants.chargeableTransferAmountId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.chargeableTransferAmountId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.chargeableTransferAmount shouldBe Some(1)
       }
 
       "return the correct answer for Date Of Death" in {
-        val cacheMap = CacheMap("a", Map(Constants.dateOfDeathId -> JsString("2018-01-01")))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.dateOfDeathId -> JsString("2018-01-01")))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.dateOfDeath shouldBe Some(new LocalDate(2018, 1, 1))
       }
 
       "return the correct answer for Date Of Disposal" in {
-        val cacheMap = CacheMap("a", Map(Constants.dateOfDisposalId -> JsString("2018-01-01")))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.dateOfDisposalId -> JsString("2018-01-01")))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.dateOfDisposal shouldBe Some(new LocalDate(2018, 1, 1))
       }
 
       "return the correct answer for Estate Has Property" in {
-        val cacheMap = CacheMap("a", Map(Constants.estateHasPropertyId -> JsBoolean(true)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.estateHasPropertyId -> JsBoolean(true)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.estateHasProperty shouldBe Some(true)
       }
 
       "return the correct answer for Gross Estate Value" in {
-        val cacheMap = CacheMap("a", Map(Constants.grossEstateValueId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.grossEstateValueId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.grossEstateValue shouldBe Some(1)
       }
 
       "return the correct answer for Percentage Closely Inherited" in {
-        val cacheMap = CacheMap("a", Map(Constants.percentageCloselyInheritedId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.percentageCloselyInheritedId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.percentageCloselyInherited shouldBe Some(1)
       }
 
       "return the correct answer for Property Value" in {
-        val cacheMap = CacheMap("a", Map(Constants.propertyValueId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.propertyValueId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.propertyValue shouldBe Some(1)
       }
 
       "return the correct answer for Property Value After Exemption" in {
-        val cacheMap = CacheMap("a", Map(
+        val cacheMap = CacheMap(cacheMapKey, Map(
           Constants.propertyValueAfterExemptionId -> JsObject(Map(
             "value" -> JsNumber(1),
             "valueCloselyInherited" -> JsNumber(2)
@@ -137,130 +139,113 @@ class UserAnswersSpec extends UnitSpec {
       }
 
       "return the correct answer for Purpose Of Use" in {
-        val cacheMap = CacheMap("a", Map(Constants.purposeOfUseId -> JsString(Constants.planning)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.purposeOfUseId -> JsString(Constants.planning)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.purposeOfUse shouldBe Some(Constants.planning)
       }
 
       "return the correct answer for Value Of Disposed Property" in {
-        val cacheMap = CacheMap("a", Map(Constants.valueOfDisposedPropertyId -> JsNumber(1)))
+        val cacheMap = CacheMap(cacheMapKey, Map(Constants.valueOfDisposedPropertyId -> JsNumber(1)))
         val userAnswers = new UserAnswers(cacheMap)
         userAnswers.valueOfDisposedProperty shouldBe Some(1)
       }
     }
 
     "values don't exist in the cache map" must {
+      val emptyCacheMap = CacheMap(cacheMapKey, Map())
+
       "return None for Any Assets Passing To Direct Descendant" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.anyAssetsPassingToDirectDescendants shouldBe None
       }
 
       "return None for Any Brought Forward Allowance" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.anyBroughtForwardAllowance shouldBe None
       }
 
       "return None for Any Brought Forward Allowance On Disposal" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.anyBroughtForwardAllowanceOnDisposal shouldBe None
       }
 
       "return None for Any Downsizing Allowance" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.anyDownsizingAllowance shouldBe None
       }
 
       "return None for Any Exemption" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.anyExemption shouldBe None
       }
 
       "return None for Any Property Closely Inherited" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.anyPropertyCloselyInherited shouldBe None
       }
 
       "return None for Assets Passing To Direct Descendant" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.assetsPassingToDirectDescendants shouldBe None
       }
 
       "return None for Brought Forward Allowance" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.broughtForwardAllowance shouldBe None
       }
 
       "return None for Brought Forward Allowance On Disposal" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.broughtForwardAllowanceOnDisposal shouldBe None
       }
 
       "return None for Chargeable Transfer Amount" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.chargeableTransferAmount shouldBe None
       }
 
       "return None for Date Of Death" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.dateOfDeath shouldBe None
       }
 
       "return None for Date Of Disposal" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.dateOfDisposal shouldBe None
       }
 
       "return None for Estate Has Property" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.estateHasProperty shouldBe None
       }
 
       "return None for Gross Estate Value" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.grossEstateValue shouldBe None
       }
 
       "return None for Percentage Closely Inherited" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.percentageCloselyInherited shouldBe None
       }
 
       "return None for Property Value" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.propertyValue shouldBe None
       }
 
       "return None for Property Value After Exemption" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.propertyValueAfterExemption shouldBe None
       }
 
       "return None for Purpose Of Use" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.purposeOfUse shouldBe None
       }
 
       "return None for Value Of Disposed Property" in {
-        val cacheMap = CacheMap("a", Map())
-        val userAnswers = new UserAnswers(cacheMap)
+        val userAnswers = new UserAnswers(emptyCacheMap)
         userAnswers.valueOfDisposedProperty shouldBe None
       }
     }
