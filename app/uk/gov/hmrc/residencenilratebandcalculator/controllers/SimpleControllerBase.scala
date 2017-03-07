12fe968230d954bbb5da4d501e3a1b64b1e1f21a
Jenny Jones <jenny.jones@digital.hmrc.gov.uk>
2017-03-07 14:57:11 +0000

    RNRBC-227 add new parameter to SimpleControllerBase

--- app/uk/gov/hmrc/residencenilratebandcalculator/controllers/SimpleControllerBase.scala
+++ app/uk/gov/hmrc/residencenilratebandcalculator/controllers/SimpleControllerBase.scala
@@ -18,20 +18,20 @@ package uk.gov.hmrc.residencenilratebandcalculator.controllers
 
 
 import play.api.data.{Form, FormError}
-import play.api.i18n.I18nSupport
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.{Reads, Writes}
 import play.api.mvc._
 import play.twirl.api.HtmlFormat
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.residencenilratebandcalculator.connectors.SessionConnector
-import uk.gov.hmrc.residencenilratebandcalculator.models.UserAnswers
+import uk.gov.hmrc.residencenilratebandcalculator.models.{AnswerRow, AnswerRows, UserAnswers}
 import uk.gov.hmrc.residencenilratebandcalculator.{FrontendAppConfig, Navigator}
 
 import scala.concurrent.Future
 
 trait ControllerBase[A] extends FrontendController with I18nSupport {
-  def onPageLoad(implicit rds: Reads[A]): Action[AnyContent]
+  def onPageLoad(implicit rds: Reads[A], messagesApi: MessagesApi): Action[AnyContent]
 
   def onSubmit(implicit wts: Writes[A]): Action[AnyContent]
 }
@@ -46,30 +46,40 @@ trait SimpleControllerBase[A] extends ControllerBase[A] {
 
   def form: () => Form[A]
 
-  def view(form: Option[Form[A]], backUrl: String)(implicit request: Request[_]): HtmlFormat.Appendable
+  def view(form: Option[Form[A]], backUrl: String, answerRows: Seq[AnswerRow])(implicit request: Request[_]): HtmlFormat.Appendable
 
   val navigator: Navigator
 
-  override def onPageLoad(implicit rds: Reads[A]): Action[AnyContent] = Action.async { implicit request =>
+  override def onPageLoad(implicit rds: Reads[A], messagesApi: MessagesApi): Action[AnyContent] = Action.async { implicit request =>
     sessionConnector.fetch().map {
       case None => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
       case Some(cacheMap) => {
-        Ok(view(cacheMap.getEntry(controllerId).map(value => form().fill(value)), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url))
+        val answerRows: Seq[AnswerRow] = AnswerRows.constructAnswerRows(
+          AnswerRows.truncateAndLocateInCacheMap(controllerId, cacheMap),
+          AnswerRows.answerRowFns,
+          AnswerRows.rowOrder,
+          messagesApi.preferred(request)
+        )
+        Ok(view(cacheMap.getEntry(controllerId).map(value => form().fill(value)), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url, answerRows))
       }
     }
   }
 
   def onSubmit(implicit wts: Writes[A]) = Action.async { implicit request =>
+    val answerRows: Seq[AnswerRow] = Seq()
     sessionConnector.fetch().flatMap {
       case None => Future.successful(Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad()))
       case Some(cacheMap) => {
         val boundForm = form().bindFromRequest()
         boundForm.fold(
           (formWithErrors: Form[A]) =>
-            Future.successful(BadRequest(view(Some(formWithErrors), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url))),
+            Future.successful(BadRequest(view(Some(formWithErrors),
+              navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url, answerRows))),
           (value) => validate(value).flatMap {
-            case Some(error) =>
-              Future.successful(BadRequest(view(Some(form().fill(value).withError(error)), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url)))
+            case Some(error) => {
+              Future.successful(BadRequest(view(Some(form().fill(value).withError(error)),
+                navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url, answerRows)))
+            }
             case None =>
               sessionConnector.cache[A](controllerId, value).map(cacheMap =>
                 Redirect(navigator.nextPage(controllerId)(new UserAnswers(cacheMap))))
