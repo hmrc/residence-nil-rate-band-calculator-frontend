17e4cc44b7d05c7e9afeae0687e3ed9d5289c550
Andy Dwelly <andydwelly@gmail.com>
2017-03-14 09:04:30 +0000

    RNRBC-239: Added quote trimming to produce valid url.(WIP)

--- app/uk/gov/hmrc/residencenilratebandcalculator/controllers/BroughtForwardAllowanceController.scala
+++ app/uk/gov/hmrc/residencenilratebandcalculator/controllers/BroughtForwardAllowanceController.scala
@@ -18,6 +18,7 @@ package uk.gov.hmrc.residencenilratebandcalculator.controllers
 
 import javax.inject.{Inject, Singleton}
 
+import play.api.Logger
 import play.api.data.{Form, FormError}
 import play.api.i18n.MessagesApi
 import play.api.libs.json.{Reads, Writes}
@@ -31,8 +32,8 @@ import uk.gov.hmrc.residencenilratebandcalculator.forms.NonNegativeIntForm
 import uk.gov.hmrc.residencenilratebandcalculator.models.UserAnswers
 import uk.gov.hmrc.residencenilratebandcalculator.views.html.brought_forward_allowance
 import uk.gov.hmrc.residencenilratebandcalculator.{Constants, FrontendAppConfig, Navigator}
-import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @Singleton
@@ -51,7 +52,7 @@ class BroughtForwardAllowanceController @Inject()(val appConfig: FrontendAppConf
   }
 
   private def getDateOfDeath(implicit hc: HeaderCarrier): Future[String] = sessionConnector.fetch().map {
-    case Some(cacheMap) => cacheMap.data(Constants.dateOfDeathId).toString()
+    case Some(cacheMap) => cacheMap.data(Constants.dateOfDeathId).toString().replaceAll("\"", "")
     case None => throw new RuntimeException("Dod problem")
   }
 
@@ -68,64 +69,27 @@ class BroughtForwardAllowanceController @Inject()(val appConfig: FrontendAppConf
 
   def onPageLoad(implicit rds: Reads[Int]) = Action.async {
     implicit request => {
-      microserviceValues.map { res => {
-        val nilRateBand = res._1.json.toString()
-        val cacheMap = res._2
-        Ok(view(cacheMap.getEntry(controllerId).map(value => form().fill(value)), navigator.lastPage(controllerId).toString(), nilRateBand))
-      }
+      microserviceValues.map {
+        case (nilRateValueJson, cacheMap) => {
+          val nilRateBand = nilRateValueJson.json.toString()
+          Ok(view(cacheMap.getEntry(controllerId).map(value => form().fill(value)), navigator.lastPage(controllerId).toString(), nilRateBand))
+        }
       } recover {
-        case n: NoCacheMapException => ???
-        case r: RuntimeException => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
+        case n: NoCacheMapException => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
+        case r: RuntimeException => {
+          Logger.error(r.getMessage, r)
+          throw r
+        }
       }
     }
   }
 
-  //  def onSubmit(implicit wts: Writes[Int]) = Action.async { implicit request => {
-  //
-  //    val result = for {
-  //      dateOfDeath <- getDateOfDeath
-  //      nilRateValueJson <- rnrbConnector.getNilRateBand(dateOfDeath)
-  //    } yield nilRateValueJson
-  //
-  //    sessionConnector.fetch().flatMap {
-  //      case None => Future.successful(Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad()))
-  //      case Some(cacheMap) => {
-  //        val boundForm = form().bindFromRequest()
-  //        boundForm.fold(
-  //          (formWithErrors: Form[Int]) => {
-  //            result.map { res => {
-  //              val nilRateBand = res.json.toString()
-  //              BadRequest(view(Some(formWithErrors), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url, nilRateBand))}
-  //            } recover {
-  //              case _ => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
-  //            }
-  //          },
-  //          (value) => validate(value).flatMap {
-  //            case Some(error) => {
-  //              result.map { res =>
-  //                val nilRateBand = res.json.toString()
-  //                BadRequest(view(Some(form().fill(value).withError(error)), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url, nilRateBand))
-  //              } recover {
-  //                case _ => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
-  //              }
-  //            }
-  //            case None =>
-  //              sessionConnector.cache[Int](controllerId, value).map(cacheMap =>
-  //                Redirect(navigator.nextPage(controllerId)(new UserAnswers(cacheMap))))
-  //          }
-  //        )
-  //      }
-  //    }
-  //  }
-  //  }
-
   def onSubmit(implicit wts: Writes[Int]) = Action.async {
     implicit request => {
       microserviceValues.flatMap {
-        res => {
+        case (nilRateValueJson, cacheMap) => {
           val boundForm = form().bindFromRequest()
-          val nilRateBand = res._1.json.toString()
-          val cacheMap = res._2
+          val nilRateBand = nilRateValueJson.json.toString()
           boundForm.fold(
             formWithErrors => Future.successful(BadRequest(view(Some(formWithErrors), navigator.lastPage(controllerId)(new UserAnswers(cacheMap)).url, nilRateBand))),
             (value) => {
@@ -136,12 +100,15 @@ class BroughtForwardAllowanceController @Inject()(val appConfig: FrontendAppConf
             }
           )
         } recover {
-          case n: NoCacheMapException => ???
-          case r: RuntimeException => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
+          case n: NoCacheMapException => Redirect(uk.gov.hmrc.residencenilratebandcalculator.controllers.routes.SessionExpiredController.onPageLoad())
+          case r: RuntimeException => {
+            Logger.error(r.getMessage, r)
+            throw r
+          }
         }
       }
     }
   }
 
   def validate(value: Int)(implicit hc: HeaderCarrier): Future[Option[FormError]] = Future.successful(None)
-  }
+}
