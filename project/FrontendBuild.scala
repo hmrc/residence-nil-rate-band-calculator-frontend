62847b8323441f4f3e441d3dbb2df789f4482469
Pankaj Khare <pankaj.khare@digital.hmrc.gov.uk>
2017-05-24 15:57:49 +0100

    RNRBC-433 upgrading play config version

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -21,7 +21,7 @@ private object AppDependencies {
   private val playUiVersion = "7.2.0"
   private val playPartialsVersion = "5.2.0"
   private val playAuthorisedFrontendVersion = "6.2.0"
-  private val playConfigVersion = "3.0.0"
+  private val playConfigVersion = "4.3.0"
   private val hmrcTestVersion = "2.1.0"
   private val scalaTestVersion = "2.2.6"
   private val pegdownVersion = "1.6.0"
