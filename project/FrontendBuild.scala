e18b45c07bfa1aed5289e24d01a36fea4e1acf5a
vineet.tyagi <vineet.tyagi@digital.hmrc.gov.uk>
2017-05-25 17:03:37 +0100

    RNRBC-454 upgrade frontend-bootstrap and modify logger level

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -16,7 +16,7 @@ private object AppDependencies {
 
   private val playHealthVersion = "2.1.0"
   private val logbackJsonLoggerVersion = "3.1.0"
-  private val frontendBootstrapVersion = "7.19.0"
+  private val frontendBootstrapVersion = "7.25.0"
   private val govukTemplateVersion = "5.1.0"
   private val playUiVersion = "7.2.1"
   private val playPartialsVersion = "5.3.0"
