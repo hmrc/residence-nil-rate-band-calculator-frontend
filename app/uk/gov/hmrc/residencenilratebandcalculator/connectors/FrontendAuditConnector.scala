683878d3a3e3a2bbbe4ed142f7deee183b778846
Jenny Jones <jenny.jones@digital.hmrc.gov.uk>
2017-10-04 11:32:07 +0100

    IHT-5150 Compilation error

--- app/uk/gov/hmrc/residencenilratebandcalculator/connectors/FrontendAuditConnector.scala
+++ app/uk/gov/hmrc/residencenilratebandcalculator/connectors/FrontendAuditConnector.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.residencenilratebandcalculator.connectors
+package uk.gov.hmrc.residencenilratebandcalculator
 
 import javax.inject.{Inject, Singleton}
 
