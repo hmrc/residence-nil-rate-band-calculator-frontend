64f5d481a33b812df2721c5dd6ca1a1acb125475
Chris Poole <CEPoole@users.noreply.github.com>
2019-01-31 09:29:35 +0000

    DL-1507 Dependency uplift

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -1,7 +1,4 @@
 import sbt._
-import uk.gov.hmrc.SbtAutoBuildPlugin
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
-import uk.gov.hmrc.versioning.SbtGitVersioning
 
 object FrontendBuild extends Build with MicroService {
 
@@ -11,19 +8,19 @@ object FrontendBuild extends Build with MicroService {
 }
 
 private object AppDependencies {
-  import play.sbt.PlayImport._
   import play.core.PlayVersion
+  import play.sbt.PlayImport._
 
-  private val bootstrapVersion = "4.7.0"
+  private val bootstrapVersion = "4.8.0"
   private val playPartialsVersion = "6.3.0"
   private val hmrcTestVersion = "3.4.0-play-25"
-  private val scalaTestVersion = "3.0.0"
+  private val scalaTestVersion = "3.0.5"
   private val pegdownVersion = "1.6.0"
-  private val mockitoCoreVersion = "2.13.0"
+  private val mockitoCoreVersion = "2.23.4"
   private val whitelistVersion = "2.0.0"
   private val httpCachingClientVersion = "8.0.0"
   private val playReactivemongoVersion = "6.2.0"
-  private val pdfBoxVersion = "2.0.4"
+  private val pdfBoxVersion = "2.0.13"
   private val playLanguageVersion = "3.4.0"
   private val scalatestplus = "2.0.1"
 
@@ -31,7 +28,7 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
     ws,
     "uk.gov.hmrc" %% "govuk-template" % "5.27.0-play-25",
-    "uk.gov.hmrc" %% "play-ui" % "7.27.0-play-25",
+    "uk.gov.hmrc" %% "play-ui" % "7.31.0-play-25",
     "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrapVersion,
     "uk.gov.hmrc" %% "play-partials" % playPartialsVersion,
     "uk.gov.hmrc" %% "play-whitelist-filter" % whitelistVersion,
@@ -51,7 +48,7 @@ private object AppDependencies {
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
-        "org.jsoup" % "jsoup" % "1.10.2" % scope,
+        "org.jsoup" % "jsoup" % "1.11.3" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
         "org.mockito" % "mockito-core" % mockitoCoreVersion % scope
       )
