72122b90d3714b0eeda6cd4e91468b9ab0935b5b
bruno sterner <47660124+uno1bruno2@users.noreply.github.com>
2019-08-21 10:56:26 +0100

    Simple reactivemongo updated to 7.20 + dependency updates

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -10,29 +10,29 @@ object FrontendBuild extends Build with MicroService {
 private object AppDependencies {
   import play.sbt.PlayImport._
 
-  private val bootstrapVersion = "0.39.0"
-  private val hmrcTestVersion = "3.8.0-play-26"
-  private val jsonJodaVersion = "2.6.10"
-  private val scalaTestVersion = "3.0.5"
+  private val bootstrapVersion = "0.45.0"
+  private val hmrcTestVersion = "3.9.0-play-26"
+  private val jsonJodaVersion = "2.6.13"
+  private val scalaTestVersion = "3.0.8"
   private val pegdownVersion = "1.6.0"
-  private val mockitoCoreVersion = "2.27.0"
+  private val mockitoCoreVersion = "2.28.2"
   private val httpCachingClientVersion = "8.3.0"
-  private val playReactivemongoVersion = "7.14.0-play-26"
-  private val pdfBoxVersion = "2.0.13"
+  private val playReactivemongoVersion = "7.20.0-play-26"
+  private val pdfBoxVersion = "2.0.16"
   private val playLanguageVersion = "3.4.0"
   private val scalatestplus = "2.0.1"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "simple-reactivemongo" % playReactivemongoVersion,
     ws,
-    "uk.gov.hmrc" %% "govuk-template" % "5.34.0-play-26",
-    "uk.gov.hmrc" %% "play-ui" % "7.39.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template" % "5.37.0-play-26",
+    "uk.gov.hmrc" %% "play-ui" % "7.40.0-play-26",
     "com.typesafe.play" %% "play-json-joda" % jsonJodaVersion,
     "uk.gov.hmrc" %% "bootstrap-play-26" % bootstrapVersion,
     "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
     "org.apache.pdfbox" % "pdfbox" % pdfBoxVersion,
     "uk.gov.hmrc" %% "play-language" % playLanguageVersion,
-    "uk.gov.hmrc" %% "http-verbs" % "9.7.0-play-26",
+    "uk.gov.hmrc" %% "http-verbs" % "9.8.0-play-26",
     nettyServer
   )
 
@@ -47,7 +47,7 @@ private object AppDependencies {
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.pegdown" % "pegdown" % pegdownVersion % scope,
-        "org.jsoup" % "jsoup" % "1.11.3" % scope,
+        "org.jsoup" % "jsoup" % "1.12.1" % scope,
         "org.mockito" % "mockito-core" % mockitoCoreVersion % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % scope
       )
