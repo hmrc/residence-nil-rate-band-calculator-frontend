aed1ce167407c3f0797116045d436e1e25d6e1d7
danny-waughman <63302027+danny-waughman@users.noreply.github.com>
2020-06-23 09:30:08 +0100

    DL-3675 Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.26.0-play-26",
-    "uk.gov.hmrc"       %% "govuk-template"       % "5.48.0-play-26",
-    "uk.gov.hmrc"       %% "play-ui"              % "8.10.0-play-26",
+    "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.27.0-play-26",
+    "uk.gov.hmrc"       %% "govuk-template"       % "5.55.0-play-26",
+    "uk.gov.hmrc"       %% "play-ui"              % "8.11.0-play-26",
     "com.typesafe.play" %% "play-json-joda"       % "2.8.1",
     "uk.gov.hmrc"       %% "bootstrap-play-26"    % "1.8.0" ,
-    "uk.gov.hmrc"       %% "http-caching-client"  % "9.0.0-play-26",
-    "org.apache.pdfbox" % "pdfbox"                % "2.0.18",
-    "uk.gov.hmrc"       %% "play-language"        % "4.2.0-play-26",
+    "uk.gov.hmrc"       %% "http-caching-client"  % "9.1.0-play-26",
+    "org.apache.pdfbox" % "pdfbox"                % "2.0.20",
+    "uk.gov.hmrc"       %% "play-language"        % "4.3.0-play-26",
     "uk.gov.hmrc"       %% "http-verbs"           % "10.5.0-play-26",
     nettyServer
   )
@@ -23,7 +23,7 @@ private object AppDependencies {
   }
 
   object Test {
-    def apply() = new TestDependencies {
+    def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
         "uk.gov.hmrc"             %% "hmrctest"           % "3.9.0-play-26"       % scope,
         "org.scalatest"           %% "scalatest"          % "3.0.8"               % scope,
