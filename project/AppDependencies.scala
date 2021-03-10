2e32ceef13430fb75927c46852031dde2d17a9fb
AhmadNaeemQA <Ahmadzeya.Naeem@digital.hmrc.gov.uk>
2021-03-10 11:37:35 +0000

    Updated copyright year

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,8 +6,8 @@ private object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc"       %% "simple-reactivemongo" % "7.30.0-play-26",
-    "uk.gov.hmrc"       %% "govuk-template"       % "5.55.0-play-26",
-    "uk.gov.hmrc"       %% "play-ui"              % "8.12.0-play-26",
+    "uk.gov.hmrc"       %% "govuk-template"       % "5.57.0-play-26",
+    "uk.gov.hmrc"       %% "play-ui"              % "8.13.0-play-26",
     "com.typesafe.play" %% "play-json-joda"       % "2.8.1",
     "uk.gov.hmrc"       %% "bootstrap-play-26"    % "1.14.0" ,
     "uk.gov.hmrc"       %% "http-caching-client"  % "9.1.0-play-26",
@@ -29,7 +29,7 @@ private object AppDependencies {
         "org.scalatest"           %% "scalatest"          % "3.0.8"               % scope,
         "org.pegdown"             % "pegdown"             % "1.6.0"               % scope,
         "org.jsoup"               % "jsoup"               % "1.13.1"              % scope,
-        "org.mockito"             % "mockito-core"        % "3.2.4"               % scope,
+        "org.mockito"             % "mockito-core"        % "3.3.3"               % scope,
         "org.scalatestplus.play"  %% "scalatestplus-play" % "3.1.3"               % scope
       )
     }.test
