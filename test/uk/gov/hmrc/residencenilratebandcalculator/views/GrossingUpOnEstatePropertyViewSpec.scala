771fae56683f4b65fe5e739c08799536d1863a1d
Pankaj Khare <12529426+pankaj2453@users.noreply.github.com>
2022-01-05 14:09:17 +0000

    DL-6983 grossing_up_on_estate_property.scala.html

--- test/uk/gov/hmrc/residencenilratebandcalculator/views/GrossingUpOnEstatePropertyViewSpec.scala
+++ test/uk/gov/hmrc/residencenilratebandcalculator/views/GrossingUpOnEstatePropertyViewSpec.scala
@@ -21,7 +21,7 @@ import uk.gov.hmrc.residencenilratebandcalculator.controllers.GrossingUpOnEstate
 import uk.gov.hmrc.residencenilratebandcalculator.controllers.routes._
 import uk.gov.hmrc.residencenilratebandcalculator.views.html.grossing_up_on_estate_property
 
-class GrossingUpOnEstatePropertyViewSpec extends BooleanViewSpecBase {
+class GrossingUpOnEstatePropertyViewSpec extends NewBooleanViewSpecBase {
 
   val messageKeyPrefix = "grossing_up_on_estate_property"
   val grossing_up_on_estate_property = injector.instanceOf[grossing_up_on_estate_property]
