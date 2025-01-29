8caf3f6a5503a7aed032b8dd3edc533d50538d1b
kwame-f-manso-acc <171707914+kwame-f-manso-acc@users.noreply.github.com>
2025-01-29 15:56:16 +0000

    DL-15309: Updated readme and Upgraded the appDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt._
 
 private object AppDependencies {
 
-  val bootstrapPlayVersion = "9.6.0"
-  val hmrcMongoVersion = "2.3.0"
+  val bootstrapPlayVersion = "9.7.0"
+  val hmrcMongoVersion = "2.4.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.8.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "11.11.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "org.apache.pdfbox"  % "pdfbox"                     % "2.0.28"
   )
